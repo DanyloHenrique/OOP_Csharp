c86b11e85e86d11daf8986572d5dd95a3f44b75c
Danylo Henrique <danylohenrique2407@gmail.com>
2026-06-05 11:04:04 -0300

    fix(Graducao.cs): repair constructor of class Graduacao

--- src/models/Graducao.cs
+++ src/models/Graducao.cs
@@ -5,7 +5,7 @@ namespace ModelGraduacao;
 
 // usando herança
 //graduacao é um tipo de Curso
-public class Graduacao(string nome, int cargaHoraria, Turma primeiraTurma int semestres) : Curso(nome, cargaHoraria, primeiraTurma)
+public class Graduacao(string nome, int cargaHoraria, Turma primeiraTurma, int semestres) : Curso(nome, cargaHoraria, primeiraTurma)
 {
     public int Semestres {get;set;} = semestres;
 }
